90784dd6742d77abdba6728d879d99608280c6c3
loana <laschleichgarcia@estudiantes.unsam.edu.ar>
2023-05-26 04:29:37 -0300

    cambios en los calculos

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,56 +1,81 @@
 import numpy as np
 import pandas as pd
 
-descomposicion_cartel = """
+DESCOMPOSICION_CARTEL = """
  __________________________
 |                          |  
 | DESCOMPOSICION DE JACOBI |
 |__________________________|
                         """
-resolucion_cartel = """
+RESOLUCION_CARTEL = """
  _________________________________
 |                                 |  
 | RESOLUCION DE LA DESCOMPOSICION |
 |_________________________________|
                         """
 
+DATAFRAME_VECTOR = pd.DataFrame()
+
 def jacobi(A, B, n, k):
-    global descomposicion_cartel
-    global resolucion_cartel
+    global DESCOMPOSICION_CARTEL
+    global RESOLUCION_CARTEL
 
     L, D, U = calculador_LDU(A)
     H, v = calculador_Hv(L, D, U, B)
-    x = np.zeros(n)
+    x = np.ones(n)
 
     dict_LDU = {' MATRIZ L:': L,' MATRIZ D:': D,' MATRIZ U:': U}
-    dict_Hv = {' MATRIZ H:': H, ' VECTOR v:': v}
+    dict_Hv = {' MATRIZ H:': H, ' MATRIZ v:': v}
 
-    imprimir_matrices_formateadas(descomposicion_cartel, dict_LDU)
-    imprimir_matrices_formateadas(resolucion_cartel, dict_Hv)
+    imprimir_matrices_formateadas(DESCOMPOSICION_CARTEL, dict_LDU)
+    imprimir_matrices_formateadas(RESOLUCION_CARTEL, dict_Hv)
 
-    iterador_jacobi(H, v, x, k)
+    return iterador_jacobi(A, H, v, x, k)
 
-def iterador_jacobi(H, v, x, k):
-    for i in range(k):
-        pass
+def iterador_jacobi(A, H, v, x0, k):
+    global DATAFRAME_VECTOR
+    for i in range(1, k+1):
+        x1 = np.sum((H * x0) + v, axis = 1)
+        DATAFRAME_VECTOR = actualizar_dataframe(DATAFRAME_VECTOR, i, x0, x1, np.sum(A, axis = 1))
+        x0 = x1
+        
+    return DATAFRAME_VECTOR
 
+# Calcula las siguientes matrices (en el mismo orden):
+# L (estrictamente triangular inferior)
+# D (diagonal) 
+# U (estrictamente triangular superior)
 def calculador_LDU(A):
     return (np.tril(A, k = -1), np.diag(np.diag(A)), np.triu(A, k = 1))
 
+# Calcula las matrices H y v producto de operaciones entre las matrices L, D, U, B
 def calculador_Hv(L, D, U, B):
-    minus_D_inverse = -np.linalg.inv(D)
-    H = np.round(minus_D_inverse + (L + U), decimals=2)
-    v = np.round(minus_D_inverse * B, decimals=2)
+    D_inverse = np.linalg.inv(D)
+    H = -1 * D_inverse * (L + U)
+    v = D_inverse * B
     return (H, v)
 
+# Imprime un título y matrices con sus respectivos subtitulos
 def imprimir_matrices_formateadas(titulo, dict_formateado):
     print(titulo)
     for (nombre, matriz) in dict_formateado.items():
         print(nombre)
         imprimir_matriz(matriz)
 
+# Imprime matriz columna por columna
 def imprimir_matriz(matrix):
     for row in matrix:
         print('  ' + str(row))
     print()
+
+# Actualiza los valores del DataFrame con los resultados de la ecuación matricial
+# y su evaluación en la matriz inicial.
+def actualizar_dataframe(df, i, x, Ax):
+    dic = pd.DataFrame({
+        'Iteración': i,
+        'Matriz aproximada': str(x),
+        'Evaluación aproximada': str(Ax)
+    } , index = range(1))
+   
+    return pd.concat([df, dic], axis = 0, ignore_index = True)
 
--- src/main.py
+++ src/main.py
@@ -3,26 +3,26 @@ import numpy as np
 from exceptions import is_column_size_different, is_zero_or_natural
 from algorithms import jacobi, imprimir_matriz
 
-metodo_cartel = """
+METODO_CARTEL = """
  _____________________
 |                     |
 | METODO DE JACOBI    |
 |_____________________|
                 """
 
-A_matrix = np.array([[1,2,3],[4,5,6],[7,8,9]])
-B_vector = np.array([11,22,33])
+A_matrix = np.array([[10,2,-3],[4,7,-1],[-2,1,4]])
+B_vector = np.array([1,-1,5])
 
-A_shape = A_matrix.shape[0]
-B_shape = B_vector.shape[0]
-is_column_size_different(A_shape, B_shape)
+A_SHAPE = A_matrix.shape[0]
+B_SHAPE = B_vector.shape[0]
+is_column_size_different(A_SHAPE, B_SHAPE)
 
-print(metodo_cartel)
+print(METODO_CARTEL)
 
 print(' MATRIZ A:')
 imprimir_matriz(A_matrix)
 
-print(' VECTOR B:')
+print(' MATRIZ B:')
 print('  ' + str(B_vector))
 print()
 
@@ -31,4 +31,8 @@ k_iters = int(input(' |_k: ')) # Número de iteraciones
 is_zero_or_natural(k_iters)
 print()
 
-jacobi(A_matrix, B_vector, A_shape, k_iters)
+datos_vector = jacobi(A_matrix, B_vector, A_SHAPE, k_iters)
+print(datos_vector)
+
+print("\nSolución final aproximada: ")
+print("Solución mediante un método exacto: " + str(np.linalg.solve(A_matrix, B_vector)))
