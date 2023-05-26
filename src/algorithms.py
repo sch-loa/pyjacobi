1163f8be8fc6492fcca5db852459288f3d5c1015
loana <laschleichgarcia@estudiantes.unsam.edu.ar>
2023-05-26 01:17:14 -0300

    agego metodos para formatear matrices + modularizo metodo de jacobi

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,19 +1,56 @@
 import numpy as np
+import pandas as pd
 
-def jacobi(A, B, k):
-    L, D, U = L_D_U_calculator(A)
-    H, v = H_v_calculator(L, D, U, B)
+descomposicion_cartel = """
+ __________________________
+|                          |  
+| DESCOMPOSICION DE JACOBI |
+|__________________________|
+                        """
+resolucion_cartel = """
+ _________________________________
+|                                 |  
+| RESOLUCION DE LA DESCOMPOSICION |
+|_________________________________|
+                        """
 
+def jacobi(A, B, n, k):
+    global descomposicion_cartel
+    global resolucion_cartel
 
+    L, D, U = calculador_LDU(A)
+    H, v = calculador_Hv(L, D, U, B)
+    x = np.zeros(n)
 
-def L_D_U_calculator(A):
+    dict_LDU = {' MATRIZ L:': L,' MATRIZ D:': D,' MATRIZ U:': U}
+    dict_Hv = {' MATRIZ H:': H, ' VECTOR v:': v}
+
+    imprimir_matrices_formateadas(descomposicion_cartel, dict_LDU)
+    imprimir_matrices_formateadas(resolucion_cartel, dict_Hv)
+
+    iterador_jacobi(H, v, x, k)
+
+def iterador_jacobi(H, v, x, k):
+    for i in range(k):
+        pass
+
+def calculador_LDU(A):
     return (np.tril(A, k = -1), np.diag(np.diag(A)), np.triu(A, k = 1))
 
-def H_v_calculator(L, D, U, B):
+def calculador_Hv(L, D, U, B):
     minus_D_inverse = -np.linalg.inv(D)
-    return (minus_D_inverse + (L + U), minus_D_inverse * B)
+    H = np.round(minus_D_inverse + (L + U), decimals=2)
+    v = np.round(minus_D_inverse * B, decimals=2)
+    return (H, v)
 
-def print_matrix(matrix):
+def imprimir_matrices_formateadas(titulo, dict_formateado):
+    print(titulo)
+    for (nombre, matriz) in dict_formateado.items():
+        print(nombre)
+        imprimir_matriz(matriz)
+
+def imprimir_matriz(matrix):
     for row in matrix:
-        print(row)
+        print('  ' + str(row))
+    print()
 
--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
 import numpy as np
 
 from exceptions import is_column_size_different, is_zero_or_natural
-from algorithms import jacobi, print_matrix
+from algorithms import jacobi, imprimir_matriz
+
+metodo_cartel = """
+ _____________________
+|                     |
+| METODO DE JACOBI    |
+|_____________________|
+                """
 
 A_matrix = np.array([[1,2,3],[4,5,6],[7,8,9]])
 B_vector = np.array([11,22,33])
@@ -10,16 +17,18 @@ A_shape = A_matrix.shape[0]
 B_shape = B_vector.shape[0]
 is_column_size_different(A_shape, B_shape)
 
-print("MATRIZ A")
-print_matrix(A_matrix)
+print(metodo_cartel)
+
+print(' MATRIZ A:')
+imprimir_matriz(A_matrix)
+
+print(' VECTOR B:')
+print('  ' + str(B_vector))
 print()
 
-print("VECTOR B")
-print(B_vector)
-print()
-
-print("NUMERO DE ITERACIONES")
-k_iters = int(input("|_k: ")) # Número de iteraciones
+print(' NUMERO DE ITERACIONES:')
+k_iters = int(input(' |_k: ')) # Número de iteraciones
 is_zero_or_natural(k_iters)
+print()
 
 jacobi(A_matrix, B_vector, A_shape, k_iters)
